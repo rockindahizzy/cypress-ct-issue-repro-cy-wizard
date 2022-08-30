ecc444a6f0fcff6601bf53a8c08545a61d01e037
Liam Butler <liam.butler@lego.com>
2022-08-30 16:18:06 +0200

    Fixing cypress spec

--- apps/site/cypress/component/App.cy.ts
+++ apps/site/cypress/component/App.cy.ts
@@ -1,9 +1,7 @@
-import React from 'react';
 import { App } from '../../src/app/app';
-import { mount } from '@cypress/react';
 
 describe('ComponentName.cy.js', () => {
   it('playground', () => {
-    mount(<App />);
+    cy.mount(App);
   });
 });
